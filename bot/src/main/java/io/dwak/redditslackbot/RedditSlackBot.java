18ad4990883a37aa626c01187f47dec44f3d15e9
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2016-12-28 02:26:22 -0500

    render a form for entering reddit info

--- bot/src/main/java/io/dwak/redditslackbot/RedditSlackBot.java
+++ bot/src/main/java/io/dwak/redditslackbot/RedditSlackBot.java
@@ -1,5 +1,6 @@
 package io.dwak.redditslackbot;
 
+import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
 import io.dwak.redditslackbot.inject.component.DaggerMainComponent;
 import io.dwak.redditslackbot.inject.component.MainComponent;
@@ -8,12 +9,13 @@ public class RedditSlackBot {
     public static final boolean DEBUG = true;
     public static void main(String[] args) throws LoadingException{
         MainComponent mainComponent = DaggerMainComponent.create();
-//        HttpService.boot(mainComponent.bot(), "reddit-slack-bot", args);
 
         if(DEBUG){
             System.out.println(mainComponent.redditConfig());
             System.out.println(mainComponent.slackConfig());
             System.out.println(mainComponent.firebaseConfig());
         }
+
+        HttpService.boot(mainComponent.bot(), "reddit-slack-bot", args);
     }
 }
--- bot/src/main/kotlin/io/dwak/redditslackbot/slack/SlackBot.kt
+++ bot/src/main/kotlin/io/dwak/redditslackbot/slack/SlackBot.kt
@@ -2,6 +2,7 @@ package io.dwak.redditslackbot.slack
 
 import io.dwak.redditslackbot.database.DbHelper
 import io.dwak.redditslackbot.inject.annotation.qualifier.slack.SlackConfig
+import io.dwak.redditslackbot.inject.module.config.ConfigValues
 import io.dwak.redditslackbot.slack.network.SlackOauthService
 import io.dwak.redditslackbot.slack.network.SlackService
 import javax.inject.Inject
@@ -9,7 +10,12 @@ import javax.inject.Singleton
 
 @Singleton
 class SlackBot @Inject constructor(val slackService: SlackService,
-                                   val slackOauthService: SlackOauthService,
+                                   val loginService: SlackOauthService,
                                    val dbHelper: DbHelper,
                                    @SlackConfig val config: Map<String, String>) {
+  fun login(code: String) {
+    loginService.getOauth(config[ConfigValues.Slack.CLIENT_ID]!!,
+        config[ConfigValues.Slack.CLIENT_SECRET]!!, code)
+        .filter { it.accessToken == config[ConfigValues.Slack.VERIFICATION_TOKEN]!! }
+  }
 }
